665a6e95d17f4c6c0bb00a841c5bbbee5504a8fc
yasmine.amr1 <yasmineabdulfattah@gmail.com>
2024-12-18 00:03:44 +0200

    feat: implement caching upon fetching weather from third party

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from "@nestjs/common";
+import { Inject } from "@nestjs/common";
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+import { Cache } from 'cache-manager';
+
 import { AxiosService } from "src/axios/axios.service";
+
 
 @Injectable({})
 export class WeatherService {
-    constructor(private readonly axiosService:AxiosService) {}
+    constructor(
+        private readonly axiosService:AxiosService,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
+    ) {}
 
-    getWeatherByCity(city) {
+    _construct_weather_cache_key(city: string) {
+        return `weather:${city.toLowerCase()}`;
+    }
+
+    async getWeatherByCity(city: string) {
+        if (!city)
+            return
+
+        const cacheKey = this._construct_weather_cache_key(city);
+        const cachedWeather: string | null = await this.cacheManager.get(cacheKey);
+
+        if (cachedWeather) {
+            console.log('Retrieved from cache');
+            return JSON.parse(cachedWeather as string);
+        }
+
         const url = process.env.WEATHER_BASE_URL + "/current.json?key=" + process.env.WEATHER_API_KEY + "&q=" + city;
-        return this.axiosService.get(url);
+        const currentWeather = await this.axiosService.get(url);
+
+        // TODO: Compute TTL to be till EOD
+        await this.cacheManager.set(cacheKey, JSON.stringify(currentWeather));
+
+        return currentWeather;
     }
 }
