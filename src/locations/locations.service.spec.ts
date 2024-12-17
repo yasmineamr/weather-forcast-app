8e24bcccc842aeff96de796ae683534b0f79989f
Yasmine Abdulfattah <yasmineabdulfattah@gmail.com>
2024-12-17 23:43:55 +0400

    Tests

--- src/locations/locations.service.spec.ts
+++ src/locations/locations.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { AddLocationDto } from './dtos/AddLocation.dto';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+import { User } from 'src/user/entities/user.entity';
 
 describe('LocationsService', () => {
   let service: LocationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+      providers: [
+        {
+          provide: LocationsService,
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
